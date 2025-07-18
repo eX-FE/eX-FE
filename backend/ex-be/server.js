e81263a58a731cd4b21a0a390792d2b52924685a
JaniyaClemmons <jac2405@columbia.edu>
2025-07-18 11:06:12 +0200

    tweets frontend logic added and cors

--- backend/ex-be/server.js
+++ backend/ex-be/server.js
@@ -3,6 +3,13 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+// Add CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get('/tweets', (req, res) => {
   res.json([
     { id: 1, user: 'elon', text: 'Building rockets 🚀' },
